64b06dd2011eea62232c2808d42532a605f1b3fc
hodket <Zalromaithi123@gmail.com>
2025-11-15 13:14:06 +0400

    Commit new file
    Added message listener to handle 'requestLastExport' action and forward it to the active tab.

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
-console.log("Background running...");
+// Background used for popup <> content messaging when popup cannot reach content
+chrome.runtime.onMessage.addListener((msg, sender, sendResp) => {
+if (msg.action === 'requestLastExport') {
+// forward to active tab content script to get last exported macro
+chrome.tabs.query({active:true,currentWindow:true}, tabs => {
+if (!tabs[0]) { sendResp({}); return; }
+chrome.tabs.sendMessage(tabs[0].id, {action: 'giveLastExport'}, (res) => {
+sendResp(res || {});
+});
+});
+// indicate we'll respond asynchronously
+return true;
+}
+});
